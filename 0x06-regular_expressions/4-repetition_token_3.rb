385c41902d7e377db0b8c035e1237646704b6232
Lisahkamau <lisahkamau@gmail.com>
2024-02-27 01:04:51 -0800

    repetion 4

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if the correct number of command-line arguments is provided
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Get the input string from the command-line argument
+input_string = ARGV[0]
+
+# Define the regular expression with repetition token
+regex = /hbt+n/
+
+# Check if there is a match using the regular expression
+match_result = input_string.match(regex)
+
+# Print the match result or an empty line if no match
+puts match_result ? match_result[0] : ""
+
